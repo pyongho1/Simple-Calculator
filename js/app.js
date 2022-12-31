4afe2676eafa80ba11e891c09083b7f7210c3a28
Yong Park <pyongho0306@gmail.com>
2022-12-30 20:53:25 -0800

    Create calculator layout and add readme

---
+++ js/app.js
@@ -0,0 +1,18 @@
+const num1 = document.getElementById("num1");
+const num2 = document.getElementById("num1");
+const num3 = document.getElementById("num1");
+const num4 = document.getElementById("num1");
+const num5 = document.getElementById("num1");
+const num6 = document.getElementById("num1");
+const num7 = document.getElementById("num1");
+const num8 = document.getElementById("num1");
+const num9 = document.getElementById("num1");
+const num0 = document.getElementById("num1");
+const clearBtn = document.getElementById("num1");
+const equalsBtn = document.getElementById("num1");
+
+num1.addEventListener("click", )
+
+function calculate() {
+  if()
+}
